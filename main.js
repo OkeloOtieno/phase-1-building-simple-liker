49111ca36cecbc4da1ec442c08e2afab00affee7
Emmanuel <emmanuelokello294@gmail.com>
2024-04-03 08:56:01 +0300

    completes liker lab

--- main.js
+++ main.js
@@ -3,8 +3,28 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
+const articleLikers = document.querySelectorAll(".like-glyph");
+function showLike(event){
+  const liker = event.target
+  mimicServerCall('http://mimicServer.example.com')
+  .then(()=>{
+    if(liker.innerText === EMPTY_HEART){
+      liker.innerText = FULL_HEART
+      liker.className = 'activated-heart'
+    }else{
+      liker.innerText = EMPTY_HEART
+      liker.className = ''
+    }
+  }).catch((err)=>{
+    const modal = document.getElementById('modal')
+    modal.className = ''
+    modal.innerText = err
+    setTimeout(()=>modal.className = 'hidden', 3000)
+  })
+}
+for (const like of articleLikers) {
+  like.addEventListener('click', showLike)
+}
 
 
 //------------------------------------------------------------------------------
